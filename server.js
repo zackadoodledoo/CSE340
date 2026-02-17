369425671e78dd82ba1121e976bed10b46d5e5a6
zackadoodledoo <sin12014@byui.edu>
2026-02-16 16:07:14 -0800

    Right before ejs renaming

--- server.js
+++ server.js
@@ -1,32 +1,58 @@
 // Imports
 // Import express using ESM syntax
+import { fileURLToPath } from 'url';
+import path from 'path';
 import express from 'express';
 
 /**
  * Declare Important Variables
  */
 // Start the server and listen on the specified port
-
+const NODE_ENV = process.env.NODE_ENV || 'production';
 const PORT = process.env.PORT || 3000;
+
+
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 /**
  * Setup Express Server
  */
 const app = express();
 
-const name = process.env.NAME; //NEWWWW
 /**
- * Declare Routes
+ * Configure Express middleware
  */
 
-// Define a route handler for the root URL ('/')
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Set EJS as the templating engine
+app.set('view engine', 'ejs');
+
+// Tell Express where to find your templates
+app.set('views', path.join(__dirname, 'src/views'));
+
+const name = process.env.NAME; //NEWWWW
+/**
+ * Declare Routes replaced by Routes (below)
+ */
+
+/**
+ * Routes -- new
+ */
 app.get('/', (req, res) => {
-    res.send(`Welcome, ${name}!`);
+    res.sendFile(path.join(__dirname, 'src/views/home.html'));
 });
 
-//New route to demonstrate dynamic content
-app.get('/new-route', (req, res) => {
-    res.send('This is a new route!');
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src/views/about.html'));
+});
+
+app.get('/products', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src/views/products.html'));
 });
 
 
